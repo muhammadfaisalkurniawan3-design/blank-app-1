76f42c70b1e8be98cd4b52be8de5f1faff02cc2e
muhammadfaisalkurniawan3-design <muhammadfaisalkurniawan3@gmail.com>
2026-05-18 11:29:33 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("🎈 JUAL ACA BELI LELE")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "ACA RAJA TANAH BARU"
 )
